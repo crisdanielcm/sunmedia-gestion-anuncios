956527822483adefe351c3128d54e4217cdb1a0d
Cristian Cruz <criscruz1806@gmail.com>
2019-10-30 17:24:00 -0500

    Creación inicial migraciones bases de datos

---
+++ app/Http/Controllers/AdComponentController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\AdComponent;
+use Illuminate\Http\Request;
+
+class AdComponentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\AdComponent  $adComponent
+     * @return \Illuminate\Http\Response
+     */
+    public function show(AdComponent $adComponent)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\AdComponent  $adComponent
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(AdComponent $adComponent)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\AdComponent  $adComponent
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, AdComponent $adComponent)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\AdComponent  $adComponent
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(AdComponent $adComponent)
+    {
+        //
+    }
+}
---
+++ database/factories/AdComponentFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\AdComponent;
+use Faker\Generator as Faker;
+
+$factory->define(AdComponent::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
